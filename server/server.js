dfd8d351cadb048f8f8c2d505522a1e425b25710
bipul276 <bipulnandan276@gmail.com>
2025-03-19 00:43:08 +0530

    changed google account key

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ const fs = require("fs");
 const connectDB = require("./config/db");
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require("./config/serviceAccountKey.json");
+
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "virmemorylane.firebasestorage.app",
